63304b685c475a1c9adee6043ff7e8b20dbc335f
Erik Kamp <ekamp@javascript.rutgers.edu>
2013-02-12 19:17:17 -0500

    Minor changes, messing around and created the adding of edges and creation of the 2d array

--- main.c
+++ main.c
@@ -4,58 +4,60 @@
 
 int main(int argc, char **argv)
 {
-    char *projectnumber = argv[1]; //This is the project that we want to run
-    char *inputFile = argv[2];
-    int projectnum = atoi(projectnumber);
-    FILE *indexFile;
-    size_t length = 0 ;
-    char *temp;
-    int count = 0;
-    struct adjLL *storage;
-    struct  node *nodeone;
-    struct node *nodetwo;
-    printf("Project number %d\n",projectnum);
-    printf("Input File: %s\n",inputFile);
-    indexFile = fopen(inputFile,"r");
-    if (indexFile == NULL)
-    {
-        printf("ERROR: Invalid filename \n");
-        return 0;
-    }
-    while(getline(&temp,&length,indexFile) != -1)
-    {
-        if (count == 0)
-        {
-            printf("Threads or Processes = %s",temp);
-        }
-        else if (count == 1)
-        {
-            printf("Number of edges = %s",temp);
-        }
-        else
-        {
-            //creating a node
-            nodeone = createNode(strtok(temp," "));
-	    if(count == 2)
-	    {
-		storage = createAdjLL(nodeone);
-	    }
-	    if(alreadyInList(storage,nodeone) == 0)
-	    {
-            	nodeone = createNode(strtok(NULL," "));
-	    }
-            //add a node to the node list if its not already there
-            //check to see if its there or not
-	    if (alreadyInList(storage,nodetwo) == 0)
-	    {
-	    	addNodeToList(storage,nodetwo);
-            }
-	    //make an edgebetween nodeone and nodetwo
-	    createEdge(nodeone,nodetwo);
-            printf("Edge = %s",temp);
-        }
-        count++;
-    }
-    fclose(indexFile);
-    return 0;
+	char *projectnumber = argv[1]; //This is the project that we want to run
+	char *inputFile = argv[2];
+	int projectnum = atoi(projectnumber);
+	FILE *indexFile;
+	size_t length = 0 ;
+	char *temp;
+	int count = 0;
+	//struct adjLL *storage;
+	struct  node *nodeone;
+	struct node *nodetwo;
+	struct node **twoDArray;
+	int numberOfEdges = 0;
+	printf("Project number %d\n",projectnum);
+	printf("Input File: %s\n",inputFile);
+	indexFile = fopen(inputFile,"r");
+	if (indexFile == NULL)
+	{
+		printf("ERROR: Invalid filename \n");
+		return 0;
+	}
+	while(getline(&temp,&length,indexFile) != -1)
+	{
+		if (count == 0)
+		{
+			printf("Threads or Processes = %s",temp);
+		}
+		else if (count == 1)
+		{
+			//Create a 2d array to store the edges and verticies
+			printf("Number of edges = %s",temp);
+			numberOfEdges = atoi(temp);
+			twoDArray = createArray(numberOfEdges);
+		}
+		else
+		{
+			//creating a node
+			nodeone = createNode(strtok(temp," "));
+			//can just use an if statement if we store the edges based on their number
+			if(alreadyInList(storage,nodeone) == 0)
+			{
+				nodeone = createNode(strtok(NULL," "));
+			}
+			//add a node to the node list if its not already there
+			//check to see if its there or not
+			if (alreadyInList(storage,nodetwo) == 0)
+			{
+				addNodeToList(twoDArray,nodetwo,numberOfEdges);
+			}
+			//make an edgebetween nodeone and nodetwo
+			createEdge(nodeone,nodetwo);
+			printf("Edge = %s",temp);
+		}
+		count++;
+	}
+	fclose(indexFile);
+	return 0;
 }
--- opperations.c
+++ opperations.c
@@ -18,25 +18,26 @@ struct adjLL * createAdjLL(struct node *firstNode)
      return newadjLL;
 }
 
-//Adds a connecting node to the end of a list
-struct node *createEdge(struct node *endOfCurrentList , struct node *newNode)
+int createEdge(struct node **array, int currentx,int currenty,struct node *newEdge)
 {
-	endOfCurrentList->next = newNode;
+	//endOfCurrentList->next = newNode;
         //the new node is now the end of the list
-	return newNode;
+	array[currentx][currenty] = newEdge;
+	return 0;
 }
 
-//Adds a new node to the list of nodes
-void addNodeToList(struct adjLL *endOfList , struct node *newNode)
+//Adds a new node to the 2d array
+void addNodeToList(struct node **array , struct node *newNode,int currentPos)
 {
-	endOfList->nextY = newNode;
+	//endOfList->nextY = newNode;
         //the new node is now the end of the list
+        array[currentPos][0] = newNode; 
 }
 
-int alreadyInList(struct adjLL * head , struct node *checkThisNode)
+int alreadyInList(/*struct adjLL * head , struct node *checkThisNode*/struct node * matchingNode , struct node **array)
 {
 	//list should just be a pointer to the list should not loose track of the list itself
-	struct adjLL *ListPointer = head;
+	/*struct adjLL *ListPointer = head;
 	while(ListPointer != NULL)
 	{
 		if (strcmp(ListPointer->current->name,checkThisNode->name) == 0)
@@ -46,8 +47,17 @@ int alreadyInList(struct adjLL * head , struct node *checkThisNode)
 		}
 	}
 	//Not in the list so add it!
+	return -1;*/
+	for(int i = 0; i < array.length(); i++)
+	{
+		if (strcmp(array[i]->name,matchingNode->name) == 0)
+		{
+			return 0;
+		}
+	}
 	return -1;
 }
+
 struct node **createArray(int numOfElements)
 {
 	int i;
